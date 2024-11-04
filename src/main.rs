6597abe0bf72cf1f4b4cb1645ab40afd3376c397
Robin313k <107352159+Robin313k@users.noreply.github.com>
2024-11-05 00:01:52 +0100

    Update main.rs
    Added functionality to print out more than one file and seperating them with banners showing the name of the file

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,26 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() > 1 {
+    if args.len() == 2 {
         let file_path: &String = &args[1];
 
         let contents = fs::read_to_string(file_path)
             .expect("Crab couldn't find the specified file!");
 
         print!("{contents}");
+    } else if args.len() > 2 {
+        for i in 1..args.len() {
+            let file_path: &String = &args[i];
+
+            let contents = fs::read_to_string(file_path)
+                .expect("Crab couldn't find the specified file!");
+            
+            println!("###########");
+            println!("{file_path}");
+            println!("###########");
+            print!("{contents}");
+        }
     } else {
-        println!("Oh noes! No input was given, therefore crab can't present Anything :(\nDid you mean to specify a file?")
+        println!("Oh no! No input was given, therefore crab can't present Anything :(\nDid you mean to specify a file?")
     }
 }
